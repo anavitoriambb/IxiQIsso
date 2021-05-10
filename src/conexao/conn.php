eb6036d6b37266e894d3021602aa57da6f91a6b8
anavitoriambb <ana.barbosa217@etec.sp.gov.br>
2021-05-10 17:08:49 -0300

    commit anagi

---
+++ src/conexao/conn.php
@@ -0,0 +1,9 @@
+<?php
+
+    // Declarar as variáveis necessárias para gerar a minha conexão com o banco de dados...
+    $hostname = "fdb29.awardspace.net";
+    $dbname = "3804792_refugiopet";
+    $username = "3804792_refugiopet";
+    $password = "Refugiopet_123";
+
+    
