3735ec8993f3609ab1e22c1310ce85eab1350a50
giovanamds <giovana.silva247@etec.sp.gov.br>
2021-06-21 15:03:57 -0300

    Commit projeto - 21.06.2021

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -1,10 +1,16 @@
 <?php
 
     // Declarar as variáveis necessárias para gerar a minha conexão com o banco de dados...
-    $hostname = "fdb29.awardspace.net";
-    $dbname = "3804792_refugiopet";
-    $username = "3804792_refugiopet";
-    $password = "Refugiopet_123";
+    //$hostname = "fdb29.awardspace.net";
+    //$dbname = "3804792_refugiopet";
+    //$username = "3804792_refugiopet";
+    //$password = "Refugiopet_123";
+
+
+    $hostname = "sql107.epizy.com";
+    $dbname = "epiz_28941053_refugiopet";
+    $username = "epiz_28941053";
+    $password = "j8TNmmUdpBgG";
 
     try {
         $pdo = new PDO('mysql:host='.$hostname.';dbname='.$dbname, $username, $password);
