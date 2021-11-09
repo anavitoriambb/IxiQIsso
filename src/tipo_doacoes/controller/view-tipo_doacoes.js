cbd3ef305eaae09fbd88943da0712b6f2c85c977
giovanamds <giovana.silva247@etec.sp.gov.br>
2021-11-09 11:32:19 -0300

    Commit projeto - 09.11.2021

---
+++ src/tipo_doacoes/controller/view-tipo_doacoes.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    $('#table-tipo').on('click', 'button.btn-view', function(e) {
+
+        e.preventDefault()
+
+        // Limpar os campos da minha janela modal
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+
+        // Criar um novo título para minha janela modal
+        $('.modal-title').append('Visualização do tipo de doação')
+
+        let IDTIPO_DOACOES = `IDTIPO_DOACOES=${$(this).attr('id')}`
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            assync: true,
+            data: IDTIPO_DOACOES,
+            url: "src/tipo_doacoes/model/view-tipo_doacoes.php",
+            success: function(dado) {
+                if (dado.tipo == "success") {
+                    $('.modal-body').load('src/tipo_doacoes/view/form-tipo_doacoes.html', function() {
+                        $('#DESCRICAO').val(dado.dados.DESCRICAO)
+                        $('#DESCRICAO').attr('readonly', 'true')
+                    })
+                    $('.btn-save').hide()
+                    $('#modal-tipo_doacoes').modal('show')
+                }
+            }
+        })
+    })
+})
