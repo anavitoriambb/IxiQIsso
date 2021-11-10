3a235e600f0d9b892b1d26d5c544633bdcdadb1f
anavitoriambb <ana.barbosa217@etec.sp.gov.br>
2021-11-10 13:33:34 -0300

    teste login git ana

--- js/tcc.js
+++
@@ -1,6 +0,0 @@
-const setDirection = document.getElementById("writing-mode");
-const content = document.getElementById("page-content");
-
-setDirection.addEventListener("change", (e) => {
-    content.setAttribute("dir", e.target.value);
-});
